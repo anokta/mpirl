ac584202a007c0c041d19fb175497d2a73e597f9
Alper Gungormusler <anokta@google.com>
2018-06-18 18:54:34 -0400

    refactors sequencer to simplify for use case.
    + added metronome for testing the sequencer.

---
+++ Assets/MPIRL/Scripts/Metronome.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour {
+  public Sequencer sequencer;
+
+  private AudioSource source;
+
+  private void Awake() {
+    source = GetComponent<AudioSource>();
+  }
+
+  void OnEnable() {
+    sequencer.OnNextBeat += OnNextBeat; 
+  }
+
+  void OnDisable() {
+    sequencer.OnNextBeat -= OnNextBeat;
+  }
+
+  private void OnNextBeat(int section, int bar, int beat, double dspTime) {
+    //source.Stop();
+    source.pitch = beat == 0 ? 2.0f : 1.0f;
+    source.PlayScheduled(dspTime);
+  }
+}
--- Assets/MPIRL/Scripts/Sequencer.cs
+++ Assets/MPIRL/Scripts/Sequencer.cs
@@ -3,79 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Sequencer : MonoBehaviour {
+  public delegate void SequencerEvent(int section, int bar, int beat, double dspTime);
+  public event SequencerEvent OnNextBeat;
+
   // Uses quarter notes for time signature, i.e., "numBeats/4".
   public double bpm = 120.0;
   public int numBeats = 4;
   public int numBars = 4;
+
+  private int currentBeat = 0;
+  private int currentBar = 0;
+  private int currentSection = 0;
   
-  public int CurrentBeat { get; private set; }
-  public int CurrentBar { get; private set; }
-  public int CurrentSection { get; private set; }
   public bool IsPlaying { get; private set; }
 
-  private double numSecondsPerBeat = 0;
-  private double timeOffsetSeconds = 0;
+  private double numSecondsPerBeat = 0.0;
 
-  private double lastDspTime = 0.0;
+  private double targetDspTime = 0.0;
 
+  private double lookaheadSeconds = 0.0f;
+
+  private void Awake() {
+    lookaheadSeconds = 2.0 * (double)Time.fixedUnscaledDeltaTime;
+  }
+
+  void Update() {
+    //Debug.Log(CurrentSection + " / " + CurrentBar + " / " + CurrentBeat + " -- " + timeOffsetSeconds);
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      if (IsPlaying) {
+        Stop();
+      } else {
+        Play();
+      }
+    }
+
+    if (!IsPlaying || numSecondsPerBeat <= 0.0f) {
+      return;
+    }
+
+    double currentDspTime = AudioSettings.dspTime;
+    while (currentDspTime + lookaheadSeconds >= targetDspTime) {
+      TriggerNextBeat();
+      ++currentBeat;
+      targetDspTime += numSecondsPerBeat;
+      if (numBeats != 0) {
+        currentBar += currentBeat / numBeats;
+        currentBeat %= numBeats;
+      }
+      if (numBars != 0) {
+        currentSection += currentBar / numBars;
+        currentBar %= numBars;
+      }
+    }
+  }
+  
+  void OnValidate() {
+    CalculateNumSamplesPerBeat();
+  }
 
   public void Play() {
-    CurrentBeat = 0;
-    CurrentBar = 0;
-    CurrentSection = 0;
-    timeOffsetSeconds = 0.0;
-    lastDspTime = AudioSettings.dspTime;
+    currentBeat = 0;
+    currentBar = 0;
+    currentSection = 0;
+    targetDspTime = AudioSettings.dspTime;
     IsPlaying = true;
   }
 
   public void Stop() {
     IsPlaying = false;
-    lastDspTime = 0.0f;
-  }
-
-  void Update() {
-    //Debug.Log(CurrentSection + " / " + CurrentBar + " / " + CurrentBeat + " -- " + timeOffsetSeconds);
-    //if (Input.GetKeyDown(KeyCode.Space)) {
-    //  if (IsPlaying) {
-    //    Stop();
-    //  } else {
-    //    Play();
-    //  }
-    //}
-
-    if (!IsPlaying) {
-      return;
-    }
-
-    double currentDspTime = AudioSettings.dspTime;
-    timeOffsetSeconds += currentDspTime - lastDspTime;
-    lastDspTime = currentDspTime;
-
-    double newBeats = System.Math.Floor(timeOffsetSeconds / numSecondsPerBeat);
-    CurrentBeat += (int) newBeats;
-    timeOffsetSeconds -= newBeats * numSecondsPerBeat;
-
-    if (numBeats == 0) {
-      return;
-    }
-    CurrentBar += CurrentBeat / numBeats;
-    CurrentBeat %= numBeats;
-
-    if (numBars == 0) {
-      return;
-    }
-    CurrentSection += CurrentBar / numBars;
-    CurrentBar %= numBars;
-  }
-
-  void OnValidate() {
-    CalculateNumSamplesPerBeat();
   }
 
   private void CalculateNumSamplesPerBeat() {
+    double previousTarget = targetDspTime - numSecondsPerBeat;
     numSecondsPerBeat = (bpm > 0.0) ? 60.0 / bpm : 0.0;
     if (numSecondsPerBeat > 0.0) {
-      timeOffsetSeconds -= System.Math.Floor(timeOffsetSeconds / numSecondsPerBeat) * numSecondsPerBeat;
+      double currentDspTime = AudioSettings.dspTime;
+      double timeOffset = currentDspTime - previousTarget;
+      targetDspTime = currentDspTime + numSecondsPerBeat - (timeOffset - System.Math.Floor(timeOffset / numSecondsPerBeat) * numSecondsPerBeat);
+    }
+  }
+
+  private void TriggerNextBeat() {
+    if (OnNextBeat != null) {
+      OnNextBeat(currentSection, currentBar, currentBeat, targetDspTime); 
     }
   }
 }
