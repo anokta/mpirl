5ea6c2ea34156efda3315d1f37f633072c243fa9
Alper Gungormusler <anokta@google.com>
2018-06-21 13:02:33 -0400

    decouples virtual objects rendering from the main camera.

---
+++ Assets/MPIRL/Scripts/VirtualObjectsRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualObjectsRenderer : MonoBehaviour {
+  public Camera mainCamera;
+  public Camera rendererCamera;
+
+  public RenderTexture rendererTexture;
+
+  private Rect rendererWindow;
+
+  void Start() {
+    int width = Screen.width;
+    int height = Screen.height;
+    rendererWindow = new Rect(0.0f, 0.0f, width, height);
+    rendererTexture.width = width;
+    rendererTexture.height = height;
+    rendererCamera.targetTexture = rendererTexture;
+  }
+
+  void Update() {
+    rendererCamera.projectionMatrix = mainCamera.projectionMatrix;
+  }
+
+  void OnGUI() {
+    GUI.DrawTexture(rendererWindow, rendererTexture);
+  }
+}
